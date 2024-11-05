be148b750c90aeba577962c484081842bbc464e5
vllxrl <124093000+vllxrl@users.noreply.github.com>
2024-11-05 20:59:06 +0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(Ticket)
+admin.site.register(Event)
+admin.site.register(TicketEvent)
---
+++ app/management/commands/utils.py
@@ -0,0 +1,43 @@
+import random
+from datetime import datetime, timedelta
+from django.utils import timezone
+
+
+def random_date():
+    now = datetime.now(tz=timezone.utc)
+    return now + timedelta(random.uniform(-1, 0) * 100)
+
+
+def random_timedelta(factor=100):
+    return timedelta(random.uniform(0, 1) * factor)
+
+
+def random_bool():
+    return bool(random.getrandbits(1))
+
+
+def random_text():
+    words = ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "cras", "eu", "blandit",
+           "lacus",  "vivamus", "tincidunt", "ante", "nec", "nunc", "tincidunt", "lacinia", "curabitur", "maximus",
+           "vulputate", "nisi", "vitae", "bibendum"]
+
+    text = ""
+
+    for _ in range(random.randint(1, 10)):
+        text += random.choice(words) + " "
+
+    text = text.strip().replace(text[0], text[0].upper(), 1)
+
+    return text
+
+
+def random_phone():
+    allowed_numbers = '658034'
+    indx = '+7916'
+    for i in range(6):
+        indx += allowed_numbers[random.randint(0, len(allowed_numbers) - 1)]
+    return indx
+
+
+def calc():
+    return random_phone()
